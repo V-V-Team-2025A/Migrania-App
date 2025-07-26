b22684a1ae40224ada85730743c0318cd75a502c
Bryan Guano <bryanguano@epn.edu.ec>
2025-07-26 08:14:41 -0500

    feat: hacer dinámico el título del header

--- frontend/src/common/components/Header.jsx
+++ frontend/src/common/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { IoArrowBack } from 'react-icons/io5';
 
-export default function Header({ title, onBack, primaryButtonText, onPrimaryClick }) {
+export default function Header({ title, onBack, primaryButtonText, onPrimaryClick, patientName }) {
+    const displayTitle = patientName ? `${title} de ${patientName}` : title;
+
     return (
         <header>
             <button className='btn-back' onClick={onBack}>
                 <IoArrowBack />
                 Volver
             </button>
-            <h1>{title}</h1>
+            <h1>{displayTitle}</h1>
             {primaryButtonText && (
                 <button className='btn-primary' onClick={onPrimaryClick}>
                     {primaryButtonText}
