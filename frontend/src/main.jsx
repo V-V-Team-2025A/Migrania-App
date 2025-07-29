2b35c5f9baf48615b3e2f0cd101d26a97bdbf5f7
gabriel.jaya@epn.edu.ec <eliath.velasco@gmail.com>
2025-07-29 10:52:42 -0500

    Feat: Se crearon las vistas del dashboard del paciente y la evaluacion Midas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import "./common/styles/index.css"
-
+import Midas from "./features/feature_Grupo1_EvaluacionMidas/pages/Midas.jsx"
+import Resultados from "./features/feature_Grupo1_EvaluacionMidas/pages/Resultados.jsx"
+import Dashboard from './pages/paciente/Dashboard.jsx'
 import Login from './pages/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -11,6 +13,9 @@ createRoot(document.getElementById('root')).render(
     <Router>
       <Routes>
         <Route path="/" element={<Login />}/>
+        <Route path="/dashboard-paciente" element={<Dashboard />}/>
+        <Route path="/midas" element={<Midas />}/>
+        <Route path="/midas/resultados" element={<Resultados />}/>
       </Routes>
     </Router>
   </StrictMode>,
---
+++ frontend/src/pages/paciente/Dashboard.jsx
@@ -0,0 +1,93 @@
+import styles from "../../common/styles/dashboardPaciente.module.css";
+import { BellIcon, ChartLineIcon, FilesIcon, PillIcon, PlusIcon } from "@phosphor-icons/react";
+
+export default function Dashboard() {
+    return (
+        <>
+            <div className={styles["dashboard__cabecera"]}>
+                <div>
+                    <h2>¿Cómo te sientes hoy?</h2>
+                    <p>¡Vas 5 días sin episodios! Sigue cuidándote y registrando tus síntomas.</p>
+                </div>
+                <BellIcon size={32} color={"var(--color-text)"} />
+            </div>
+            <section className={styles["dashboard__contenedor-tarjetas"]}>
+                <div className={styles["dashboard__tarjeta"]}>
+                    <div
+                        className={styles["dashboard__icono"]}
+                        style={{
+                            backgroundColor: "#c42d3750",
+                        }}>
+                        <PlusIcon size={32} color={"#c9525aff"} />
+                    </div>
+                    <h4>Registrar episodio</h4>
+                    <p>Documenta un nuevo episodio de migraña.</p>
+                </div>
+                <div className={styles["dashboard__tarjeta"]}>
+                    <div
+                        className={styles["dashboard__icono"]}
+                        style={{
+                            backgroundColor: "#9f58dd59",
+                        }}>
+                        <FilesIcon size={32} color={"#a555ebff"} />
+                    </div>
+                    <h4>Registrar episodio</h4>
+                    <p>Documenta un nuevo episodio de migraña.</p>
+                </div>
+                <div className={styles["dashboard__tarjeta"]}>
+                    <div
+                        className={styles["dashboard__icono"]}
+                        style={{
+                            backgroundColor: "#3e68be50",
+                        }}>
+                        <PillIcon size={32} color={"#4c84f3ff"} />
+                    </div>
+                    <h4>Registrar episodio</h4>
+                    <p>Documenta un nuevo episodio de migraña.</p>
+                </div>
+                <div className={styles["dashboard__tarjeta"]}>
+                    <div
+                        className={styles["dashboard__icono"]}
+                        style={{
+                            backgroundColor: "#3588517d",
+                        }}>
+                        <ChartLineIcon size={32} color={"#42b668ff"} />
+                    </div>
+                    <h4>Mi progreso</h4>
+                    <p>Visualiza tu progreso a lo largo del tiempo.</p>
+                </div>
+            </section>
+
+            <div className={styles["dashboard__seccion-inferior"]}>
+                <section className={styles["dashboard__episodios-recientes"]}>
+                    <h4>Episodios recientes</h4>
+
+                </section>
+
+                <section className={styles["dashboard__mi-medico"]}>
+                    <h4>Mi médico</h4>
+
+                    <div className={styles["dashboard__medico-info"]}>
+                        <img height={64} width={64} src="https://media.istockphoto.com/id/177373093/photo/indian-male-doctor.jpg?s=612x612&w=0&k=20&c=5FkfKdCYERkAg65cQtdqeO_D0JMv6vrEdPw3mX1Lkfg="
+                            alt="Doctor" />
+                        <h4>Dr. Juan Pérez</h4>
+                        <p>Especialista en Neurología</p>
+                    </div>
+
+                    <div>
+                        <h4>Próxima cita:</h4>
+                        <div style={{color: "var(--color-secondary-dark)"}}>2025-10-22 - 10:00 AM</div>
+                    </div>
+
+                    <div>
+                        <h4>Tratamiento actual:</h4>
+                        <div style={{color: "var(--color-secondary-dark)"}}>Propranolol 40mg</div>
+                    </div>
+
+                    <button className="btn-primary">Agendar</button>
+                </section>
+            </div>
+
+        </>
+    );
+}
