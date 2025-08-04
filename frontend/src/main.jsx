e49e38c0b7baac20805689308c6c5a60ded59750
Eliath Velasco <eliath.velasco@gmail.com>
2025-08-03 21:11:09 -0500

    Feat: Agregar estadisticas del midas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,29 +16,31 @@ import BDoctor from '@/features/feature_Grupo2_BitacoraAsistidaCefalea/pages/Bit
 import BPaciente from '@/features/feature_Grupo2_BitacoraAsistidaCefalea/pages/Bitacora-Paciente.jsx'
 import Registro from '@/features/feature_Grupo2_BitacoraAsistidaCefalea/pages/Registro-cefalea.jsx'
 import MidasMedico from './features/feature_Grupo1_EvaluacionMidas/pages/MidasMedico.jsx'
+import MidasEstadisticas from './features/feature_Grupo1_EvaluacionMidas/pages/MidasEstadisticas.jsx'
 
 createRoot(document.getElementById('root')).render(
 
-  <StrictMode>
-    <Router>
-      <Routes>
+    <StrictMode>
+        <Router>
+            <Routes>
                 <Route path="/bitacora-medico/:pacienteId" element={<BDoctor />} />
                 <Route path="/midas-medico/:pacienteId" element={<MidasMedico />} />
                 <Route path="/bitacora-paciente" element={<BPaciente />} />
                 <Route path="/registro" element={<Registro />} />
-        <Route path="/login" element={<Login />}/>
-          <Route path="/primerConsulta" element={<PrimerConsulta />}/>
-          <Route path="/primerConsulta/crearTratamiento" element={<CrearTratamiento />}/>
-          <Route path="/seguimiento" element={<Seguimiento />}/>
-          <Route path= "/seguimiento/crearTratamiento" element ={<CrearTratamiento />}/>
-          <Route path="/seguimiento/tratamientos" element={<Tratamientos />}/>
-          <Route path="/seguimiento/tratamientos/crearTratamiento" element={<CrearTratamiento />}/>
-        <Route path="/dashboard-paciente" element={<Dashboard />}/>
-        <Route path="/midas" element={<Midas />}/>
-        <Route path="/midas/resultados" element={<Resultados />}/>
-      </Routes>
-    </Router>
-  </StrictMode>,
+                <Route path="/login" element={<Login />} />
+                <Route path="/primerConsulta" element={<PrimerConsulta />} />
+                <Route path="/primerConsulta/crearTratamiento" element={<CrearTratamiento />} />
+                <Route path="/seguimiento" element={<Seguimiento />} />
+                <Route path="/seguimiento/crearTratamiento" element={<CrearTratamiento />} />
+                <Route path="/seguimiento/tratamientos" element={<Tratamientos />} />
+                <Route path="/seguimiento/tratamientos/crearTratamiento" element={<CrearTratamiento />} />
+                <Route path="/dashboard-paciente" element={<Dashboard />} />
+                <Route path="/midas" element={<Midas />} />
+                <Route path='/midas/estadisticas' element={<MidasEstadisticas/>}/>
+                <Route path="/midas/resultados" element={<Resultados />} />
+            </Routes>
+        </Router>
+    </StrictMode>,
 
 
 )
